bbf3f3ee3e7981c887c764dca2c77d5d32b7f0f2
Duy Huynh <huynhtruongdyu@gmail.com>
2023-12-05 17:23:38 +0700

    add thememing

---
+++ src/app/core/components/theme-switcher/theme-switcher.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-switcher',
+  standalone: true,
+  imports: [],
+  templateUrl: './theme-switcher.component.html',
+  styleUrl: './theme-switcher.component.scss'
+})
+export class ThemeSwitcherComponent {
+  public isLightTheme = true;
+  constructor() {
+    this.isLightTheme = document.body.getAttribute('data-theme') == 'light';
+  }
+
+  onThemeSwitchChange() {
+    this.isLightTheme = !this.isLightTheme;
+
+    document.body.setAttribute(
+      'data-theme',
+      this.isLightTheme ? 'light' : 'dark'
+    );
+  }
+}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ThemeSwitcherComponent } from './components/theme-switcher/theme-switcher.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ThemeSwitcherComponent
+  ]
+})
+export class CoreModule { }
